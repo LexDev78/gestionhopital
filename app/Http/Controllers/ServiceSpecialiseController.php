878f90f5f0c65b21126d4a522e959ffff80a13b8
Aissata Kaloga <aissatakaloga3@gmail.com>
2023-02-08 15:54:08 +0000

    migrations

---
+++ app/Http/Controllers/ServiceSpecialiseController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service_specialise;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ServiceSpecialiseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Service_specialise  $service_specialise
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Service_specialise $service_specialise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Service_specialise  $service_specialise
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Service_specialise $service_specialise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Service_specialise  $service_specialise
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Service_specialise $service_specialise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Service_specialise  $service_specialise
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Service_specialise $service_specialise)
+    {
+        //
+    }
+}
