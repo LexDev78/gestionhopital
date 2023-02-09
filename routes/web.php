41a4e93f580631be6e478f72701434e822ff4287
sorysissoko <ibasko3@gmail.com>
2023-02-09 17:40:22 +0000

    by sory

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware("auth")->name('home');
+
+
+//**Test */
+Route::resource('test', TestController::class);
